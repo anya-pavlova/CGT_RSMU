defc57ad3dbaa7d427127d40d7ab5cec31021a7e
AfasRob <afas1robert@gmail.com>
2019-09-06 16:52:21 +0300

    up

--- trim_fastq.py
+++ trim_fastq.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 import sys
 
+filename = sys.argv[1]
+
 if __name__ == "__main__":
-    with open(sys.argv[1], 'r') as f:
+    with open(filename, 'r') as f:
         lines = iter(f)
         while True:
             try:
